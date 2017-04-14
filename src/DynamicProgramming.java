dc023a114c20d46046606a84145a105592000294
LAPTOP-1MJEVISJ\Souparni Agnihotri <souparni@iastate.edu>
2017-04-14 16:31:12 -0500

    Commit rand

---
+++ src/DynamicProgramming.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class DynamicProgramming {
+	
+	/**
+	 * Returns a Min-cost vertical cut. Type of this method must be array
+	 * list of integers.
+	 * @param M
+	 * @return
+	 */
+	public  static ArrayList<Integer> minCostVC(int[][] M){
+		
+		
+		return null;
+		
+	}
+
+}
